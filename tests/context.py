0b6c535d3285fedfbf424b0c1286ddb7c3fb34ae
bglid <benjamin.h.glidden@gmail.com>
2024-11-04 13:51:29 -0500

    Adjusting tests for new directories

--- tests/context.py
+++ tests/context.py
@@ -3,3 +3,6 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import haitian_creole_teaching_tools  # noqa # pylint: disable=unused-import, wrong-import-position
+import models  # noqa # pylint: disable=unused-import, wrong-import-position
+import streamlit_app  # noqa # pylint: disable=unused-import, wrong-import-position
+
